732e9277b6a183defc93777f09c4bfb509266f67
sungseanpark <sungseanpark@gmail.com>
2024-05-26 18:48:42 -0700

    Complete part 9.10

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import diaryRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/api/diagnoses', diaryRouter);
 
 const PORT = 3001;
 
